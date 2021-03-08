7667c98fa09caa6fc136a7d32a9aa3f0fb569e6d
Kachanovich Vitali <maloyvital@gmail.com>
2021-03-08 21:10:45 +0300

    add Form and FilterButton components

--- src/index.js
+++ src/index.js
@@ -3,5 +3,11 @@ import {render} from 'react-dom';
 import App from './components/App';
 import './components/App.css';
 
+const DATA = [
+    { id: "todo-0", name: "Eat", completed: true },
+    { id: "todo-1", name: "Sleep", completed: false },
+    { id: "todo-2", name: "Repeat", completed: false }
+];
 
-render(<App />, document.getElementById('root'));
+
+render(<App tasks={DATA}/>, document.getElementById('root'));
